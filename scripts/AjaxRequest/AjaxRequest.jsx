ad6a8bc1ffcb42962d967afdcfd83e74774f1a25
Rob <robert.carr@owasp.org>
2019-01-30 23:19:49 +0000

    Remove dynamic serialisation of function to prevent breakage in prod build

--- scripts/AjaxRequest/AjaxRequest.jsx
+++ scripts/AjaxRequest/AjaxRequest.jsx
@@ -20,5 +20,26 @@ function ajaxRequest (method, url, data, cb) {
 }
 
 const fn = ajaxRequest
-const implementation = ajaxRequest.toString()
+const implementation = `
+function ajaxRequest (method, url, data, cb) {
+  var xmlHttp = new XMLHttpRequest()
+
+  if (xmlHttp.overrideMimeType) {
+    xmlHttp.overrideMimeType('text/plain; charset=x-user-defined')
+  }
+
+  xmlHttp.open(method, url, true)
+
+  if (cb) {
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState === 4) {
+        cb(xmlHttp)
+      }
+    }
+  }
+
+  xmlHttp.send(data)
+  return xmlHttp
+}`
+
 export { implementation, fn }
